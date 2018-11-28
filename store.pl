4fb982a13b5e293094c742fb0d67bdaaafe047e7
Prayansh Srivastava <prayansh@gmail.com>
2018-11-28 11:23:44 -0800

    store now fills in the gaps in timeline

--- store.pl
+++ store.pl
@@ -3,6 +3,8 @@
 % store constants
 door(pos(0,0)).
 exit(pos(5,5)).
+sizeX(5).
+sizeY(5).
 
 prop(apple, name, "Apple").
 prop(apple, weight, 10).
@@ -30,34 +32,44 @@ shelf(f, carrot).
 % predicates
 item(N, W, P) :- prop(X, name, N), prop(X, weight, W), prop(X, price, P).
 
-
 % Story
 % mat a has weight 0 at time 0 at pos(0,0)
-measurement(a, 40, 0, pos(2,1)). % 4 apples
-measurement(b, 70, 0, pos(3,1)). % 7 bananas
-measurement(c, 100, 0, pos(2,2)). % 5 carrots
-measurement(d, 150, 0, pos(3,2)). % 5 dates
-measurement(e, 30, 0, pos(2,3)). % 3 apples
-measurement(f, 60, 0, pos(3,3)). % 3 carrots
+measurement_raw(a, 40, 0, pos(2,1)). % 4 apples
+measurement_raw(b, 70, 0, pos(3,1)). % 7 bananas
+measurement_raw(c, 100, 0, pos(2,2)). % 5 carrots
+measurement_raw(d, 150, 0, pos(3,2)). % 5 dates
+measurement_raw(e, 30, 0, pos(2,3)). % 3 apples
+measurement_raw(f, 60, 0, pos(3,3)). % 3 carrots
 
 % User u has entered
-measurement(u, 0, 0, pos(0,0)). % user’s mat_id=u
-measurement(u, 0, 1, pos(1,0)).
-measurement(u, 0, 2, pos(1,1)).
-measurement(u, 10, 3, pos(1,1)). % get 1 apple from a
-measurement(a, 30, 3, pos(2,1)).
+measurement_raw(u, 0, 0, pos(0,0)). % user’s mat_id=u
+measurement_raw(u, 0, 1, pos(1,0)).
+measurement_raw(u, 0, 2, pos(1,1)).
+measurement_raw(u, 10, 3, pos(1,1)). % get 1 apple from a
+measurement_raw(a, 30, 3, pos(2,1)).
 
-measurement(u, 10, 4, pos(1,2)).
-measurement(u, 10, 5, pos(1,3)).
-measurement(u, 30, 6, pos(1,3)). % get 2 apple from e
-measurement(e, 10, 6, pos(2,3)).
+measurement_raw(u, 10, 4, pos(1,2)).
+measurement_raw(u, 10, 5, pos(1,3)).
+measurement_raw(u, 30, 6, pos(1,3)). % get 2 apple from e
+measurement_raw(e, 10, 6, pos(2,3)).
 
-measurement(u, 30, 7, pos(1,4)).
-measurement(u, 30, 8, pos(2,4)).
-measurement(u, 30, 9, pos(3,4)).
-measurement(u, 40, 10, pos(3,4)).
-measurement(f, 50, 10, pos(3,3)).
+measurement_raw(u, 30, 7, pos(1,4)).
+measurement_raw(u, 30, 8, pos(2,4)).
+measurement_raw(u, 30, 9, pos(3,4)).
+measurement_raw(u, 40, 10, pos(3,4)).
+measurement_raw(f, 50, 10, pos(3,3)).
 
-measurement(u, 40, 11, pos(3,5)).
-measurement(u, 40, 12, pos(4,5)).
-measurement(u, 40, 13, pos(5,5)).
+measurement_raw(u, 40, 11, pos(3,5)).
+measurement_raw(u, 40, 12, pos(4,5)).
+measurement_raw(u, 40, 13, pos(5,5)).
+
+measurement_raw(_, 0, 0, pos(-1,-1)).
+
+% Use measurement for actually retrieving values
+measurement(X, W, T, P):- measurement_raw(X, W, T, P), !.
+measurement(X, W, T, P):- T0 is T - 1, measurement(X, W, T0, P), !.
+
+pos_in_store(X, Y):- sizeX(XS), MAX_X is XS, sizeY(YS), MAX_Y is YS, between(0,MAX_X,X), between(0,MAX_Y,Y).
+pos_in_store(pos(X, Y)):- pos_in_store(X,Y).
+
+is_in_store(M):- measurement(M, _, _, P), pos_in_store(P).
