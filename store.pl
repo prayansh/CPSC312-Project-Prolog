049e7136ef24b094edf77a35aec3f23f7b930757
guilhermelameira <gui.l.a@hotmail.com>
2018-11-29 16:40:03 -0800

    Gave more intuitive mat ids and fixed the measure mechanism to reuse past measurements

--- store.pl
+++ store.pl
@@ -22,52 +22,59 @@ prop(date, name, "Date").
 prop(date, weight, 30).
 prop(date, price, 2).
 
-shelf(a, apple).
-shelf(b, banana).
-shelf(c, carrot).
-shelf(d, date).
-shelf(e, apple).
-shelf(f, carrot).
+% shelf(id, product)
+shelf(s1, apple).
+shelf(s2, banana).
+shelf(s3, carrot).
+shelf(s4, date).
+shelf(s5, apple).
+shelf(s6, carrot).
 
 % predicates
 item(N, W, P) :- prop(X, name, N), prop(X, weight, W), prop(X, price, P).
 
 % Story
 % mat a has weight 0 at time 0 at pos(0,0)
-measurement_raw(a, 40, 0, pos(2,1)). % 4 apples
-measurement_raw(b, 70, 0, pos(3,1)). % 7 bananas
-measurement_raw(c, 100, 0, pos(2,2)). % 5 carrots
-measurement_raw(d, 150, 0, pos(3,2)). % 5 dates
-measurement_raw(e, 30, 0, pos(2,3)). % 3 apples
-measurement_raw(f, 60, 0, pos(3,3)). % 3 carrots
+% measurement_raw(id, weight, time, position)
+measurement_raw(s1, 40, 0, pos(2,1)). % 4 apples
+measurement_raw(s2, 70, 0, pos(3,1)). % 7 bananas
+measurement_raw(s3, 100, 0, pos(2,2)). % 5 carrots
+measurement_raw(s4, 150, 0, pos(3,2)). % 5 dates
+measurement_raw(s5, 30, 0, pos(2,3)). % 3 apples
+measurement_raw(s6, 60, 0, pos(3,3)). % 3 carrots
 
 % User u has entered
-measurement_raw(u, 0, 0, pos(0,0)). % user’s mat_id=u
-measurement_raw(u, 0, 1, pos(1,0)).
-measurement_raw(u, 0, 2, pos(1,1)).
-measurement_raw(u, 10, 3, pos(1,1)). % get 1 apple from a
-measurement_raw(a, 30, 3, pos(2,1)).
+measurement_raw(b1, 0, 0, pos(0,0)). % user’s mat_id=u
+measurement_raw(b1, 0, 1, pos(1,0)).
+measurement_raw(b1, 0, 2, pos(1,1)).
+measurement_raw(b1, 10, 3, pos(1,1)). % get 1 apple from a
+measurement_raw(s1, 30, 3, pos(2,1)).
 
-measurement_raw(u, 10, 4, pos(1,2)).
-measurement_raw(u, 10, 5, pos(1,3)).
-measurement_raw(u, 30, 6, pos(1,3)). % get 2 apple from e
-measurement_raw(e, 10, 6, pos(2,3)).
+measurement_raw(b1, 10, 4, pos(1,2)).
+measurement_raw(b1, 10, 5, pos(1,3)).
+measurement_raw(b1, 30, 6, pos(1,3)). % get 2 apple from e
+measurement_raw(s5, 10, 6, pos(2,3)).
 
-measurement_raw(u, 30, 7, pos(1,4)).
-measurement_raw(u, 30, 8, pos(2,4)).
-measurement_raw(u, 30, 9, pos(3,4)).
-measurement_raw(u, 40, 10, pos(3,4)).
-measurement_raw(f, 50, 10, pos(3,3)).
+measurement_raw(b1, 30, 7, pos(1,4)).
+measurement_raw(b1, 30, 8, pos(2,4)).
+measurement_raw(b1, 30, 9, pos(3,4)).
+measurement_raw(b1, 40, 10, pos(3,4)).
+measurement_raw(s6, 50, 10, pos(3,3)).
 
-measurement_raw(u, 40, 11, pos(3,5)).
-measurement_raw(u, 40, 12, pos(4,5)).
-measurement_raw(u, 40, 13, pos(5,5)).
+measurement_raw(b1, 40, 11, pos(3,5)).
+measurement_raw(b1, 40, 12, pos(4,5)).
+measurement_raw(b1, 40, 13, pos(5,5)).
 
-measurement_raw(_, 0, 0, pos(-1,-1)).
+measurement_raw(_, _, -1, pos(-1,-1)).
+% measurement(ID, W, 0, P) 
+
+measurement_raw(b2, 40, 5, pos(5,5)).
+
+
 
 % Use measurement for actually retrieving values
-measurement(X, W, T, P):- measurement_raw(X, W, T, P), !.
-measurement(X, W, T, P):- T0 is T - 1, measurement(X, W, T0, P), !.
+measurement(X, W, T, P):- measurement_raw(X, W, T, P).
+measurement(X, W, T, P):- T0 is T - 1, T >= -1,  measurement(X, W, T0, P), not(measurement_raw(X, _, T, _)).
 
 pos_in_store(X, Y):- sizeX(XS), MAX_X is XS, sizeY(YS), MAX_Y is YS, between(0,MAX_X,X), between(0,MAX_Y,Y).
 pos_in_store(pos(X, Y)):- pos_in_store(X,Y).
